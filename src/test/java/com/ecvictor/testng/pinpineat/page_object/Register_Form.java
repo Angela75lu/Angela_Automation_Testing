c0063b9ec1b7a321ff5a7344a41416400284ed13
Angela75lu <yanqinglu75@gmail.com>
2018-01-12 13:18:52 -0500

    add PinpinEatTest.java file

---
+++ src/test/java/com/ecvictor/testng/pinpineat/page_object/Register_Form.java
@@ -0,0 +1,10 @@
+package com.ecvictor.testng.pinpineat.page_object;
+
+public class Register_Form {
+    public final static String signUpLink = "//*[@id=\"loginDiv\"]/div[2]/div/form/div[3]/p[2]/a"; //link
+    public final static String userName = "fullName"; //by id
+    public final static String email ="email"; //by id
+    public final static String pass = "pass";
+    public final static String rePassword = "repassword"; //by id
+    public final static String signUpBtn = "//*[@id=\"loginDiv\"]/div[3]/div/div/form/div[5]/button"; //
+}
