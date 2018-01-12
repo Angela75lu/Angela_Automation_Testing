4e2e70534582d09ee65de493ffe198c5b2285574
Angela75lu <yanqinglu75@gmail.com>
2018-01-12 13:18:37 -0500

    add PinpinEatTest.java file

---
+++ src/test/java/com/ecvictor/testng/pinpineat/page_object/HomePageMainMenu.java
@@ -0,0 +1,8 @@
+package com.ecvictor.testng.pinpineat.page_object;
+
+public class HomePageMainMenu {
+    public final static String loginLink = "//*[@id=\"myNavBar\"]/ul/li[1]/a/span/span[2]";
+    public final static String loginBtn = "//*[@id=\"loginDiv\"]/div[2]/div/form/div[3]/button";
+    public final static String invalidLogin = "//*[@id=\"loginDiv\"]/div[2]/div/p";
+    public final static String studyHere="//a[@href=\"/future-students.html\" and @class]";
+}
